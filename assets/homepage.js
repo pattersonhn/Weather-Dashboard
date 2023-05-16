b0aa6d2b301855874f37589721413553d5e0db9e
pattersonhn <pattersonhn01@gmail.com>
2023-05-16 16:01:41 -0400

    created local variables and functions to impliment local storage

---
+++ assets/homepage.js
@@ -0,0 +1,40 @@
+var openWeatherApiKey = '6106c4d899c96f08e553721aeaa9f0fc';
+var openWeatherCoordinatesUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='
+var userFormEL = $('#city-search');
+var col2El = $('.col2');
+var cityInputEl = $('#city');
+var fiveDayEl = $('#five-day');
+var searchHistoryEl = $('#search-history');
+var currentDay = moment().format('M/DD/YYYY');
+const weatherIconUrl = 'http://openweathermap.org/img/wn/';
+var searchHistoryArray = loadSearchHistory();
+
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+
+    return splitStr.join(' ');
+}
+
+function loadSearchHistory() {
+    var searchHistoryArray = JSON.parse(localStorage.getItem('search history'));
+    if (!searchHistoryArray) {
+        searchHistoryArray = {
+            searchedCity: [],
+        };
+    } else {
+        for (var i = 0; i < searchHistoryArray.searchedCity.length; i++) {
+            searchHistory(searchHistoryArray.searchedCity[i]);
+        }
+    }
+    return searchHistoryArray;
+}
+
+function saveSearchHistory() {
+    localStorage.setItem('search history', JSON.stringify(searchHistoryArray));
+};
+
+
